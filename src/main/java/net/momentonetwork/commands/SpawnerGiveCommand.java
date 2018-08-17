4a51439d57589887605f914dd6bdda2b2c818578
danbzns <danbzns@gmail.com>
2018-08-17 00:06:49 -0400

    Add /spawnergive command

---
+++ src/main/java/net/momentonetwork/commands/SpawnerGiveCommand.java
@@ -0,0 +1,41 @@
+package net.momentonetwork.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Collections;
+
+public class SpawnerGiveCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender.hasPermission("mineablespawners.give")) {
+            if (args.length == 3) {
+                Player target = (Player) Bukkit.getPlayer(args[0]);
+                String type = args[1].toString().toLowerCase();
+                int amount = Integer.valueOf(args[2]);
+                if (target.getInventory().firstEmpty() != -1) {
+                    ItemStack item = new ItemStack(Material.SPAWNER);
+                    item.setAmount(amount);
+                    item.setLore(Collections.singletonList(ChatColor.YELLOW + "type: " + ChatColor.GRAY + type));
+                    target.getInventory().addItem(item);
+                    sender.sendMessage(ChatColor.GREEN + "Gave " + target.getDisplayName() + " " + amount + "x " + type + " spawners!");
+                    target.sendMessage(ChatColor.GREEN + "Received " + amount + "x " + type + " spawners!");
+                } else {
+                    sender.sendMessage(ChatColor.RED + "Player needs to clear up some inventory space before receiving a spawner!");
+                }
+            } else {
+                sender.sendMessage(ChatColor.RED + "Invalid format, please use: /spawnergive (name) (type) (amount).");
+            }
+        } else {
+            sender.sendMessage(ChatColor.RED + "Sorry, you do not have permission to do this!");
+        }
+        return false;
+    }
+}
