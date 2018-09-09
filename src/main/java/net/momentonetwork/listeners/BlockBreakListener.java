8216de34e41ea0d6570530c43746d04fb6f2c606
fergydanny <35203867+itsfergydanny@users.noreply.github.com>
2018-09-09 01:03:05 -0400

    added perm to break spawner

--- src/main/java/net/momentonetwork/listeners/BlockBreakListener.java
+++ src/main/java/net/momentonetwork/listeners/BlockBreakListener.java
@@ -28,27 +28,28 @@ public class BlockBreakListener implements Listener {
 
             CreatureSpawner spawner = (CreatureSpawner) block.getState();
 
-            if (player.getInventory().getItemInMainHand().containsEnchantment(Enchantment.SILK_TOUCH)) {
-
-                // Cancel exp drop
-                e.setExpToDrop(0);
-
-                // Bring block straight to inv
-                if (player.getInventory().firstEmpty() != -1) {
-                    ItemStack item = new ItemStack(material);
-                    ItemMeta meta = item.getItemMeta();
-                    meta.setLore(Collections.singletonList(ChatColor.YELLOW + "type: " + ChatColor.GRAY + spawner.getSpawnedType().toString().toLowerCase()));
-                    item.setItemMeta(meta);
-                    player.getInventory().addItem(item);
-                    block.getDrops().clear();
+            if(player.hasPermission("mineablespawners.break")) {
+                if (player.getInventory().getItemInMainHand().containsEnchantment(Enchantment.SILK_TOUCH)) {
+                    // Cancel exp drop
+                    e.setExpToDrop(0);
+                    // Bring block straight to inv
+                    if (player.getInventory().firstEmpty() != -1) {
+                        ItemStack item = new ItemStack(material);
+                        ItemMeta meta = item.getItemMeta();
+                        meta.setLore(Collections.singletonList(ChatColor.YELLOW + "type: " + ChatColor.GRAY + spawner.getSpawnedType().toString().toLowerCase()));
+                        item.setItemMeta(meta);
+                        player.getInventory().addItem(item);
+                        block.getDrops().clear();
+                    } else {
+                        player.sendMessage(ChatColor.RED + "Please clear up some inventory space before mining a spawner!");
+                        e.setCancelled(true);
+                    }
                 } else {
-                    player.sendMessage(ChatColor.RED + "Please clear up some inventory space before mining a spawner!");
+                    player.sendMessage(ChatColor.RED + "You must use a pickaxe with silk touch to mine a spawner!");
                     e.setCancelled(true);
                 }
-
-
             } else {
-                player.sendMessage(ChatColor.RED + "You must use a pickaxe with silk touch to mine a spawner!");
+                player.sendMessage(ChatColor.RED + "Sorry, you do not have permission to do this!");
                 e.setCancelled(true);
             }
         }
