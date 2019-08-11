19f942b9aa9b43691ccf23feaec70e684b7e32b9
fergydanny <35203867+itsfergydanny@users.noreply.github.com>
2019-08-10 23:10:53 -0400

    update readme

--- src/main/java/com/dnyferguson/mineablespawners/listeners/BlockBreakListener.java
+++ src/main/java/com/dnyferguson/mineablespawners/listeners/BlockBreakListener.java
@@ -9,7 +9,6 @@ import org.bukkit.block.CreatureSpawner;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
