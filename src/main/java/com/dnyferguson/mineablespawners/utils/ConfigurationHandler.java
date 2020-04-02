0fed4aa1fbda1b177dc7e1a96c5c2e4dacc4ecc9
tyldanny <99778985+tyldanny@users.noreply.github.com>
2020-04-02 00:41:22 -0400

    finish commands

--- src/main/java/com/dnyferguson/mineablespawners/utils/ConfigurationHandler.java
+++ src/main/java/com/dnyferguson/mineablespawners/utils/ConfigurationHandler.java
@@ -15,6 +15,10 @@ public class ConfigurationHandler {
 
     public ConfigurationHandler(MineableSpawners plugin) {
         FileConfiguration config = plugin.getConfig();
+        reload(config);
+    }
+
+    public void reload(FileConfiguration config) {
         globalSection(config);
         mainSection(config);
         giveSection(config);
@@ -24,18 +28,18 @@ public class ConfigurationHandler {
     private void globalSection(FileConfiguration config) {
         Map<String, String> msgs = new HashMap<>();
         Map<String, Boolean> bools = new HashMap<>();
-        Map<String, List<String>> lst = new HashMap<>();
+        Map<String, List<String>> lsts = new HashMap<>();
 
         ConfigurationSection section = config.getConfigurationSection("global");
 
         msgs.put("name", section.getString("display.name"));
 
-        lst.put("lore", section.getStringList("display.lore"));
+        lsts.put("lore", section.getStringList("display.lore"));
 
         bools.put("lore-enabled", section.getBoolean("display.lore-enabled"));
 
         messages.put("global", msgs);
-        lists.put("global", lst);
+        lists.put("global", lsts);
         booleans.put("global", bools);
     }
 
@@ -48,6 +52,7 @@ public class ConfigurationHandler {
         msgs.put("give", section.getString("helpmessage.give"));
         msgs.put("set", section.getString("helpmessage.set"));
         msgs.put("types", section.getString("helpmessage.types"));
+        msgs.put("reload", section.getString("helpmessage.reload"));
 
         messages.put("main", msgs);
     }
@@ -75,10 +80,36 @@ public class ConfigurationHandler {
     private void setSection(FileConfiguration config) {
         Map<String, String> msgs = new HashMap<>();
         Map<String, Boolean> bools = new HashMap<>();
+        Map<String, List<String>> lsts = new HashMap<>();
 
         ConfigurationSection section = config.getConfigurationSection("set");
 
         msgs.put("no-permission", section.getString("messages.no-permission"));
+        msgs.put("no-individual-permission", section.getString("messages.no-individual-permission"));
+        msgs.put("invalid-type", section.getString("messages.invalid-type"));
+        msgs.put("not-looking-at", section.getString("messages.not-looking-at"));
+        msgs.put("already-type", section.getString("messages.already-type"));
+        msgs.put("success", section.getString("messages.success"));
+        msgs.put("blacklisted", section.getString("messages.blacklisted"));
+
+        bools.put("require-permission", section.getBoolean("require-permission"));
+        bools.put("require-individual-permission", section.getBoolean("require-individual-permission"));
+
+        lsts.put("blacklisted-worlds", section.getStringList("blacklisted-worlds"));
+
+        messages.put("set", msgs);
+        booleans.put("set", bools);
+    }
+
+    private void typesSection(FileConfiguration config) {
+        Map<String, String> msgs = new HashMap<>();
+        Map<String, Boolean> bools = new HashMap<>();
+
+        ConfigurationSection section = config.getConfigurationSection("types");
+
+        msgs.put("no-permission", section.getString("messages.no-permission"));
+        msgs.put("title", section.getString("messages.title"));
+        msgs.put("entries", section.getString("messages.entries"));
 
         bools.put("require-permission", section.getBoolean("require-permission"));
 
