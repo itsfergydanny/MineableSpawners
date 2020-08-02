b8adf77583abf4ad583898f34b38c979df60ef07
Danny Ferguson <me@dnyferguson.com>
2020-08-02 16:01:57 -0400

    replace own nms handling with NBTApi and fix 1.14 support that was accidentally removed when the nms system was initially moved over

---
+++ src/main/java/com.dnyferguson.mineablespawners/MineableSpawners.java
@@ -0,0 +1,77 @@
+package com.dnyferguson.mineablespawners;
+
+import com.dnyferguson.mineablespawners.api.API;
+import com.dnyferguson.mineablespawners.commands.MineableSpawnersCommand;
+import com.dnyferguson.mineablespawners.listeners.*;
+import com.dnyferguson.mineablespawners.metrics.Metrics;
+import com.dnyferguson.mineablespawners.utils.ConfigurationHandler;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.entity.EntityType;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MineableSpawners extends JavaPlugin {
+    private ConfigurationHandler configurationHandler;
+    private Economy econ;
+    private static API api;
+
+    @Override
+    public void onEnable() {
+        getConfig().options().copyDefaults(true);
+        saveDefaultConfig();
+
+        configurationHandler = new ConfigurationHandler(this);
+
+        if (!setupEconomy()) {
+            System.out.println("[MineableSpawners] vault not found, economy features disabled.");
+        }
+
+        getCommand("mineablespawners").setExecutor(new MineableSpawnersCommand(this));
+
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(new SpawnerMineListener(this), this);
+        pm.registerEvents(new SpawnerPlaceListener(this), this);
+        pm.registerEvents(new EggChangeListener(this), this);
+        pm.registerEvents(new AnvilRenameListener(this), this);
+        pm.registerEvents(new SpawnerExplodeListener(this), this);
+
+        if (getConfigurationHandler().getBoolean("global", "show-available")) {
+            StringBuilder str = new StringBuilder("[MineableSpawners] Available mob types: \n");
+            for (EntityType type : EntityType.values()) {
+                str.append("- ");
+                str.append(type.name());
+                str.append("\n");
+            }
+            System.out.println(str.toString());
+        }
+
+        api = new API(this);
+        int pluginId = 7354;
+        Metrics metrics = new Metrics(this, pluginId);
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    public ConfigurationHandler getConfigurationHandler() {
+        return configurationHandler;
+    }
+
+    public Economy getEcon() {
+        return econ;
+    }
+
+    public static API getApi() {
+        return api;
+    }
+}
---
+++ src/main/java/com.dnyferguson.mineablespawners/commands/GiveSubCommand.java
@@ -0,0 +1,73 @@
+package com.dnyferguson.mineablespawners.commands;
+
+import com.cryptomorin.xseries.XMaterial;
+import com.dnyferguson.mineablespawners.MineableSpawners;
+import com.dnyferguson.mineablespawners.utils.Chat;
+import de.tr7zw.changeme.nbtapi.NBTItem;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GiveSubCommand {
+
+    public GiveSubCommand() {}
+
+    public void execute(MineableSpawners plugin, CommandSender sender, String target, String type, String amt) {
+        Player targetPlayer = Bukkit.getPlayer(target);
+        if (target == null || targetPlayer == null) {
+            plugin.getConfigurationHandler().sendMessage("give", "player-does-not-exist", sender);
+            return;
+        }
+
+        EntityType entityType;
+        try {
+            entityType = EntityType.valueOf(type.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            plugin.getConfigurationHandler().sendMessage("give", "invalid-type", sender);
+            return;
+        }
+
+        int amount = 0;
+        try {
+            amount = Integer.parseInt(amt);
+        } catch (NumberFormatException e) {
+            plugin.getConfigurationHandler().sendMessage("give", "invalid-amount", sender);
+            return;
+        }
+
+        if (targetPlayer.getInventory().firstEmpty() == -1) {
+            plugin.getConfigurationHandler().sendMessage("give", "inventory-full", sender);
+            return;
+        }
+
+        ItemStack item = new ItemStack(XMaterial.SPAWNER.parseMaterial());
+        ItemMeta meta = item.getItemMeta();
+        item.setAmount(amount);
+
+        String mobFormatted = Chat.uppercaseStartingLetters(entityType.name());
+        meta.setDisplayName(Chat.format(plugin.getConfigurationHandler().getMessage("global", "name").replace("%mob%", mobFormatted)));
+        List<String> newLore = new ArrayList<>();
+        if (plugin.getConfigurationHandler().getList("global", "lore") != null && plugin.getConfigurationHandler().getBoolean("global", "lore-enabled")) {
+            for (String line : plugin.getConfigurationHandler().getList("global", "lore")) {
+                newLore.add(Chat.format(line).replace("%mob%", mobFormatted));
+            }
+            meta.setLore(newLore);
+        }
+        item.setItemMeta(meta);
+
+        NBTItem nbti = new NBTItem(item);
+        nbti.setString("ms_mob", entityType.name());
+
+        item = nbti.getItem();
+
+        targetPlayer.getInventory().addItem(item);
+        plugin.getConfigurationHandler().getMessage("give", "success").replace("%mob%", mobFormatted).replace("%target%", targetPlayer.getName()).replace("%amount%", amount + "");
+        targetPlayer.sendMessage(plugin.getConfigurationHandler().getMessage("give", "received").replace("%mob%", mobFormatted).replace("%target%", targetPlayer.getName()).replace("%amount%", amount + ""));
+    }
+}
